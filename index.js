55e5702ebac505ea2ccc3474ba2bf4c371efeae0
Carlos Galindo <utm22030587@utma.edu.mx>
2024-08-06 08:57:55 -0600

    feat: add default port as 5000

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.get("/", (req, res) => {
     res.send({Hello : "World"});
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 TestProduct();
